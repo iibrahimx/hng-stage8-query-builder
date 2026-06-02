d7571b79baaacc31490d962589b346c911f07489
Ibrahim Ibrahim <eebrahiim04@gmail.com>
2026-06-02 16:34:00 +0100

    feat: update users dataset and schema

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,3 +1,7 @@
 export { usersSchema } from "./users-schema";
 export { usersDataset } from "./users-dataset";
 export type { UserRecord } from "./users-dataset";
+
+export { productsSchema } from "./products-schema";
+export { productsDataset } from "./products-dataset";
+export type { ProductRecord } from "./products-dataset";
